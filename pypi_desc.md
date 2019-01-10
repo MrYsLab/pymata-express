d4703058a200248658ed0db06aa0382caf140355
MrYsLab <MrYsLab@gmail.com>
2019-01-09 19:59:09 -0500

    Initial commit

---
+++ pypi_desc.md
@@ -0,0 +1,29 @@
+![logo](https://raw.github.com/MrYsLab/pymata-express/master/documentation/images/pymata_express.png)
+
+
+pymata-express is a high performance, non-blocking, Python asyncio client
+for the Firmata Protocol that supports the complete StandardFirmata
+protocol in addition to an enhanced of Firmata called FirmataExpress.
+
+### Major features
+==============
+
+* **Python 3.7+ compatible.**
+
+* **Fully documented [API.](https://github.com/MrYsLab/pymata-express/blob/master/documentation/api/api.md)**
+
+* **User's Guide, Including Examples.**
+
+* **Implements 100% of the StandardFirmata Protocol (StandardFirmata 2.5.8).**
+
+* **Advanced auto-detection of Arduino devices (when using FirmataExpress).**
+
+* **FirmataExpress (enhanced StandardFirmata sketch) included with distribution. It adds support for:**
+     * **HC-SR04 Ultrasonic Distance Sensors using a single pin.**
+     * **Stepper Motors.**
+     * **Piezo Tone Generation.**
+     * **Baud rate of 115200**
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open('pypi_desc.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pymata-express',
+    version='1.00',
+    packages=['pymata_express'],
+    install_requires=['pyserial'],
+    url='https://mryslab.github.io/pymata-express/',
+    download_url='https://github.com/MrYsLab/pymata-express',
+    license='GNU Affero General Public License v3 or later (AGPLv3+)',
+    author='Alan Yorinks',
+    author_email='MisterYsLab@gmail.com',
+    description='A Python Protocol Abstraction Library For Arduino Firmata using Python asyncio',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['Firmata', 'Arduino', 'Protocol', 'Python', 'asyncio'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+        'Topic :: Education',
+        'Topic :: Home Automation',
+    ],
+)
+
