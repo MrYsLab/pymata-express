133bbef5eac10673203bbdf947cd827c6f8e17c9
MrYsLab <MrYsLab@gmail.com>
2019-02-06 12:16:02 -0500

    Version 1.2

--- pymata_express/private_constants.py
+++ pymata_express/private_constants.py
@@ -69,7 +69,7 @@ class PrivateConstants:
     SYSEX_REALTIME = 0x7F  # MIDI Reserved for realtime messages
 
     # reserved for PyMata
-    PYMATA_EXPRESS_VERSION = "1.00"
+    PYMATA_EXPRESS_VERSION = "1.2"
 
     # each byte represents a digital port
     #  and its value contains the current port settings
