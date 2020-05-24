6d16110d5a4dbff0158a1725bef439ee9e5cc929
MrYsLab <MisterYsLab@gmail.com>
2020-05-24 15:25:09 -0400

    Fixed DHT11 calculations

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('pypi_desc.md', "r") as f:
 
 setup(
     name='pymata-express',
-    version='1.13',
+    version='1.14',
     packages=['pymata_express'],
     install_requires=['pyserial'],
     url='https://mryslab.github.io/pymata-express/',
